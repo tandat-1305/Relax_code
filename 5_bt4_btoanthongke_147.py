5eb9ff2a761f70679e076676982f7fff3417a2fb
tandat-1305 <72129432+tandat-1305@users.noreply.github.com>
2021-02-01 23:03:43 +0700

    Add files via upload

---
+++ 5_bt4_btoanthongke_147.py
@@ -0,0 +1,17 @@
+﻿from random import randint
+
+
+n = int(input(("Nhập số phần tử : ")))
+A = [randint(0,1000000) for i in range(n)]
+print(A," ")
+list1 = [0 for i in range(1000001)]
+
+for x in A:
+    list1[x] +=1
+
+print("Kết quả thống kê là : ")
+for i in range(1000001):
+    if list1[i] > 0:
+        print("Phần tử ",i," xuất hiện ", list1[i]," lần")
+
+
