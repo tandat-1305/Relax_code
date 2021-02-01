b780afaea72e410bd9f4c9de42786c92c8039633
tandat-1305 <72129432+tandat-1305@users.noreply.github.com>
2021-02-01 23:20:04 +0700

    Add files via upload

---
+++ 6_bt8_matranchuyenvi_151.py
@@ -0,0 +1,25 @@
+﻿m = int(input("Nhap m = "))
+n = int(input("Nhap n = "))
+print("Nhap ma tran A ",m," dong ",n," cot")
+
+A = [[int(input()) for i in range(n)] for j in range(m)]
+AT=[]
+
+for i in range(n):
+	row=[]
+	for j in range(m):
+		row.append(0)
+	AT.append(row)
+
+for i in range(n):
+	for j in range(m):
+		AT[i][j] = A[j][i]
+print("Ma tran A: ")
+
+print("Ma trận đã nhập:")
+for x in A:
+	print(x)
+
+print("Ma trận chuyển vị:")
+for x in AT:
+	print(x)
